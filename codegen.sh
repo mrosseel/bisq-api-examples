d508a0ce22f1eb541adf0c2a640b43551395211b
Mike Rosseel <mike.rosseel@gmail.com>
2017-09-29 10:04:02 +0200

    New codegen script and codegen directory
    Moved all generated apis to the /generated directory, so a new
    /custom directory can be added for custom made example api usage.
    Codegen script now uses docker so nothing needs to be downloaded,
    only prerequisite is docker, curl and a running api on localhost:8080

---
+++ codegen.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+currdir=`pwd`
+curl http://localhost:8080/swagger.json > swagger.json
+rm -Rf generated docs
+docker run -it -v ${currdir}:/tmp swaggerapi/swagger-codegen-cli generate -i /tmp/swagger.json -l html -o /tmp/docs
+docker run -it -v ${currdir}:/tmp swaggerapi/swagger-codegen-cli generate -i /tmp/swagger.json -l python -o /tmp/generated/python
+docker run -it -v ${currdir}:/tmp swaggerapi/swagger-codegen-cli generate -i /tmp/swagger.json -l java -o /tmp/generated/java
+docker run -it -v ${currdir}:/tmp swaggerapi/swagger-codegen-cli generate -i /tmp/swagger.json -l php -o /tmp/generated/php
+docker run -it -v ${currdir}:/tmp swaggerapi/swagger-codegen-cli generate -i /tmp/swagger.json -l go -o /tmp/generated/golang
+rm swagger.json
--- generate.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-docker run -it --net=host swaggerapi/swagger-codegen-cli generate -i http://localhost:8080/swagger.json -l html -o .
-#docker run -it swaggerapi/swagger-codegen-cli generate -i http://localhost:8080/swagger.json -l python -o ./python
-#docker run -it swaggerapi/swagger-codegen-cli generate -i http://localhost:8080/swagger.json -l java -o ./java
-#docker run -it swaggerapi/swagger-codegen-cli generate -i http://localhost:8080/swagger.json -l php -o ./php
-#docker run -it swaggerapi/swagger-codegen-cli generate -i http://localhost:8080/swagger.json -l go -o ./go
